860268549887eb4dba5bd421f9a80b0586b6339e
victor neves <victorneves@weg.net>
2025-04-16 14:14:21 -0300

    updated project name and envar

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -8,6 +8,7 @@ REPO_DIR = BASE_DIR.parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 TEMPLATES_DIR.mkdir(parents=True, exist_ok=True)
 
+PROJECT_NAME = config("PROJECT_NAME", default="Unset Project Name")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
--- src/cfehome/views.py
+++ src/cfehome/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
+
+PROJECT_NAME = getattr(settings, "PROJECT_NAME", "Unset Project in Views")
 
 def hello_world(request):
-    return render(request, "hello-world.html", {})
+    return render(request, "hello-world.html", {
+
+        "project_name": PROJECT_NAME,
+
+    })
 
 def healthz_view(request):
     return HttpResponse("OK")
